83c7641fe5df74b4dcd981903325f465efcdc30f
Kalir888 <kalir888@gmail.com>
2022-05-01 15:39:19 +0530

    uc 12 optimized and Lamda expressions are defined Explicitly

--- Day23Assignment/src/main/java/com/userregistration/UserRegistration.java
+++ Day23Assignment/src/main/java/com/userregistration/UserRegistration.java
@@ -1,7 +1,18 @@
 package com.userregistration;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface UserValidater {
+    String NAME_PATTERN = "^([A-Z])([a-z]{2})([a-z]*)$";
+    String EMAIL_PATTERN = "^([a-z0-9]+)([.+_-]?[a-z0-9]+)?@([a-z0-9]+)(.[a-z]{2,4})?(.[a-z]{2,4})$";
+    String PHONE_NO_PATTERN = "^91-[0-9]{10}$";
+    String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=[^!@#$%^&+=]*[!@#$%^&+=][^!@#$%^&+=]*$).{8}$";
+
+    void validate(String s) throws UserDetailException;
+}
 
 public class UserRegistration {
     private Scanner get = new Scanner(System.in);
@@ -14,53 +25,48 @@ public class UserRegistration {
         getPassWord();
     }
 
+    UserValidater nameValidator = s -> {
+        Pattern pattern = Pattern.compile(UserValidater.NAME_PATTERN);
+        if(!pattern.matcher(s).matches())
+            throw new UserDetailException("Invalid Name");};
+
+    UserValidater phoneNoValidator = s -> {
+        Pattern pattern = Pattern.compile(UserValidater.PHONE_NO_PATTERN);
+        if(!pattern.matcher(s).matches())
+            throw new UserDetailException("Invalid Phone no");};
+
+    UserValidater emailValidator = s -> {
+        Pattern pattern = Pattern.compile(UserValidater.EMAIL_PATTERN);
+        if(!pattern.matcher(s).matches())
+            throw new UserDetailException("Invalid Email Id");};
+
+    UserValidater passwordValidator = s -> {
+        Pattern pattern = Pattern.compile(UserValidater.PASSWORD_PATTERN);
+        if(!pattern.matcher(s).matches())
+            throw new UserDetailException("Invalid Password");};
+
     private void getUserFirstName() throws UserDetailException {
         System.out.println("Enter your First Name");
-        String firstName = get.next();
-        UserValidater userValidater = () -> {
-            Pattern pattern = Pattern.compile(UserValidater.NAME_PATTERN);
-            if(!pattern.matcher(firstName).matches())
-                throw new UserDetailException("Invalid First Name");};
-        userValidater.validateUserDetails();
+        nameValidator.validate(get.next());
     }
 
     private void getUserLastName() throws UserDetailException {
         System.out.println("Enter your Last Name");
-        String secondName = get.next();
-        UserValidater userValidater = () -> {
-            Pattern pattern = Pattern.compile(UserValidater.NAME_PATTERN);
-            if(!pattern.matcher(secondName).matches())
-                throw new UserDetailException("Invalid Second Name");};
-        userValidater.validateUserDetails();
+        nameValidator.validate(get.next());
     }
 
     private void getUserPhoneNo() throws UserDetailException {
         System.out.println("Enter your Phone no");
-        String phoneNo = get.next();
-        UserValidater userValidater = () -> {
-            Pattern pattern = Pattern.compile(UserValidater.PHONE_NO_PATTERN);
-            if(!pattern.matcher(phoneNo).matches())
-                throw new UserDetailException("Invalid Phone no");};
-        userValidater.validateUserDetails();
+        phoneNoValidator.validate(get.next());
     }
 
     private void getUserEmailId() throws UserDetailException {
         System.out.println("Enter your Email Id");
-        String emailId = get.next();
-        UserValidater userValidater = () -> {
-            Pattern pattern = Pattern.compile(UserValidater.EMAIL_PATTERN);
-            if(!pattern.matcher(emailId).matches())
-                throw new UserDetailException("Invalid Email Id");};
-        userValidater.validateUserDetails();
+        emailValidator.validate(get.next());
     }
 
     private void getPassWord() throws UserDetailException {
         System.out.println("Enter your PassWord");
-        String password = get.next();
-        UserValidater userValidater = () -> {
-            Pattern pattern = Pattern.compile(UserValidater.PASSWORD_PATTERN);
-            if(!pattern.matcher(password).matches())
-                throw new UserDetailException("Invalid Password");};
-        userValidater.validateUserDetails();
+        passwordValidator.validate(get.next());
     }
 }
